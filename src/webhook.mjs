b776ddb007f50cf76c8c2ed4e8fae31e0db2b10e
Markus Felten <Markus.Felten@gmx.de>
2020-11-28 21:54:08 +0100

    fix: align against http ctx

--- src/webhook.mjs
+++ src/webhook.mjs
@@ -1,16 +1,18 @@
 import { createHmac } from "crypto";
-import { Interceptor } from '@kronos-integration/interceptor';
+import { Interceptor } from "@kronos-integration/interceptor";
 
 /**
  *
  */
 export class GithubHookInterceptor extends Interceptor {
   static get name() {
-    return 'github-webhook';
+    return "github-webhook";
   }
 
-  async receive(endpoint, next, request) {
-    const [sig, event, id] = headers(request, [
+  async receive(endpoint, next, ctx) {
+    const secret = endpoint.owner.webhook.secret;
+
+    const [sig, event, id] = headers(ctx, [
       "x-hub-signature",
       "x-github-event",
       "x-github-delivery"
@@ -18,22 +20,21 @@ export class GithubHookInterceptor extends Interceptor {
 
     const body = await rawBody(ctx.req);
 
-    if (!verify(sig, body, this.secret)) {
-      throw("x-hub-signature does not match blob signature");
+    if (!verify(sig, body, secret)) {
+      throw "x-hub-signature does not match blob signature";
     }
 
     return next(JSON.parse(body.toString()), event);
   }
 }
 
-
 export class GiteaHookInterceptor extends Interceptor {
   static get name() {
-    return 'gitea-webhook';
+    return "gitea-webhook";
   }
 
-  async receive(endpoint, next, request) {
-    const [event, id] = headers(request, ["x-gitea-event", "x-gitea-delivery"]);
+  async receive(endpoint, next, ctx) {
+    const [event, id] = headers(ctx, ["x-gitea-event", "x-gitea-delivery"]);
 
     const body = await rawBody(ctx.req);
     const data = JSON.parse(body.toString());
@@ -50,14 +51,13 @@ export class GiteaHookInterceptor extends Interceptor {
   }
 }
 
-
 export class BitbucketHookInterceptor extends Interceptor {
   static get name() {
-    return 'bitbucket-webhook';
+    return "bitbucket-webhook";
   }
 
-  async receive(endpoint, next, request) {
-    const [event] = headers(request, ["X-Event-Key"]);
+  async receive(endpoint, next, ctx) {
+    const [event] = headers(ctx, ["x-event-key"]);
     const body = await rawBody(ctx.req);
     const data = JSON.parse(body.toString());
 
@@ -67,7 +67,7 @@ export class BitbucketHookInterceptor extends Interceptor {
 
 function headers(ctx, names) {
   return names.map(name => {
-    const v = ctx.get(name);
+    const v = ctx.req.headers[name];
     if (v === undefined || v.length === 0) {
       ctx.throw(400, `${name} required`);
     }
@@ -76,12 +76,17 @@ function headers(ctx, names) {
 }
 
 function sign(data, secret) {
-  return "sha1=" +
-    createHmac("sha1", secret)
-      .update(data)
-      .digest("hex");
+  return "sha1=" + createHmac("sha1", secret).update(data).digest("hex");
 }
 
 function verify(signature, data, secret) {
   return Buffer.from(signature).equals(Buffer.from(sign(data, secret)));
+}
+
+async function rawBody(req) {
+  const chunks = [];
+  for await (const chunk of req) {
+    chunks.push(chunk);
+  }
+  return chunks.join("");
 }
